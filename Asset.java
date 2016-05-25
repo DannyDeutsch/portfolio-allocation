7b521940ab767ba59be698e456f21204f445bdc5
DannyDeutsch <danielbdeutsch@gmail.com>
2016-05-25 12:49:35 -0400

    updates

--- Asset.java
+++ Asset.java
@@ -2,10 +2,13 @@ public class Asset
 {
 	public String ticker;
 	public double price;
+	public List<Asset> assets = new ArrayList<Asset>();
 
 	public Asset(String ticker)
 	{
 		this.ticker = ticker;
 		//TODO: this.price = 
+
+		assets.add(this(ticker));
 	}
 }
